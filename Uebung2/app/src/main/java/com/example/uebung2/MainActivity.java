3bc0bd2d2d71fe65e5acd1aa7398d6cb49342de2
Phex <crow@mail.de>
2022-12-03 16:48:15 +0100

    Refactor: Update for Uebung2

--- Uebung2/app/src/main/java/com/example/uebung2/MainActivity.java
+++ Uebung2/app/src/main/java/com/example/uebung2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
