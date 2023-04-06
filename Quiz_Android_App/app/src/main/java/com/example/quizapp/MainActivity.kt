e0f8a6c1f78891beff97cf19bd9d2d1781745cfe
Phex <crow@mail.de>
2023-04-06 17:49:05 +0200

    refactor: MainActivity.kt

--- Quiz_Android_App/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ Quiz_Android_App/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        btn_start.setOnClickListener {
+
+            if(et_name.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuizActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
     }
 }
